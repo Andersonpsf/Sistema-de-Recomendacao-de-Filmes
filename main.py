a961c095d753fe0411ee94ca3a46b9de30c5dd5d
Anderson Filho <andersonsfilho2006@gmail.com>
2025-06-18 18:21:18 -0300

    Exibir mensagem caso não exista o filme ou gênero pesquisado

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 import time 
-base = open("BaseDeDados.txt", "r")
+
 while True:
     print("=" * 22)
     print("Recomendação de Filmes")
     print("=" * 22)
-    print("1 - Mostrar Filmes ")
+    print("\n1 - Mostrar Filmes ")
     print("2 - Buscar por Nome")
     print("3 - Buscar por Gênero")
     print("4 - Sair")
-
+    encontrado = False
 
     escolha = int(input("Opção: "))
     if escolha ==1:
+        base = open("BaseDeDados.txt", "r")
         texto = base.read()
         print(texto)
+
     elif escolha==2:
-        
-        nome = input("Digite o nome do filme: ").lower()
+        base = open("BaseDeDados.txt", "r")
+        nome = input("\nDigite o nome do filme: ").lower()
         for line in base:
             filmes = line.split("|")
-            if nome in filmes[0].strip().lower():
+            if nome in filmes[0].lower():
+                encontrado=True
                 print(line)
+        if encontrado == False:
+            print("Filme não encontrado")
             
 
     elif escolha==3:
-        genero = input("Genero: ").lower()
+        base = open("BaseDeDados.txt", "r")
+        genero = input("\nGenero: ").lower()
         for line in base:
             filmes = line.split("|")
             if genero in  filmes[1].lower():
+                encontrado = True
                 print(line)
-            
+        if encontrado==False:
+            print("Genero não encontrado")
+
     elif escolha == 4:
         print("Você saiu do programa.")
         break
